d6192d2f20b142c2dc2727d8565dcdedb8a8f7e5
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2024-09-12 00:01:52 +0200

    [FEATURE] add transition between pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import transition from '@astrojs/transition';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://deptrai95.github.io',
   base: '/',
+  integrations: [transition()],
 });
