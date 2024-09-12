3021af6a81b23c50f0c32eda86c145175d68354a
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2024-09-12 18:25:29 +0200

    [BUGFIX] remove astro transition from astro conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import transition from '@astrojs/transition';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://deptrai95.github.io',
   base: '/',
-  integrations: [transition()],
 });
