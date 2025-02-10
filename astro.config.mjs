b0d5552f08eaf1ae8d6769f6fe32362032c40d75
Duc Anh Dennis Nguyen <dennidnguyen@yahoo.de>
2025-02-10 10:43:02 +0100

    [TASK] update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://deptrai95.github.io',
+  site: 'https://deptrai.de',
   base: '/',
   integrations: [sitemap()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 });
